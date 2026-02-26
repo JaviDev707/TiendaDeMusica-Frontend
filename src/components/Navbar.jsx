f843fc069e51ea1ec3ce177e437d95e2332d78c9
JaviDev707 <javigisgar99@gmail.com>
2026-02-26 19:31:32 +0100

    Mejoras Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaCartShopping } from "react-icons/fa6";
 import { GrLogout } from "react-icons/gr";
 import { useCart } from "../context/CartContext.jsx";
 import { useAuth } from "../context/AuthContext.jsx";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const { totalItems } = useCart();
@@ -13,18 +14,22 @@ const Navbar = () => {
       <nav className="navbar">
         <h1 className="logo">SeamLess Musical</h1>
         <ul className="nav-links">
-          <a href="/">Inicio</a>
-          <a href="/catalog">Catálogo</a>
-          <a href="/cart">
-            <FaCartShopping /> {totalItems > 0 ? ` (${totalItems})` : ""}
-          </a>
-          <a href="/profile">Perfil</a>
+          <Link to="/">Inicio</Link>
+          <Link to="/catalog">Catálogo</Link>
+          {isAuthenticated && (
+            <>
+              <Link to="/cart">
+                <FaCartShopping /> {totalItems > 0 ? ` (${totalItems})` : ""}
+              </Link>
+              <Link to="/profile">Perfil</Link>
+            </>
+          )}
           {isAuthenticated ? (
             <button onClick={logout} className="logout-button">
               <GrLogout /> 
             </button>
           ) : (
-            <a href="/login">Login</a>
+            <Link to="/login">Login</Link>
           )}
         </ul>
       </nav>
